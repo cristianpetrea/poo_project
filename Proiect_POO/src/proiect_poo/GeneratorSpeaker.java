e5c5ef3aa68c76ecf3ca0d0ba18191437220e1c3
cristianpetrea <120105706+cristianpetrea@users.noreply.github.com>
2022-12-15 15:15:17 +0200

    Update GeneratorSpeaker.java

--- Proiect_POO/src/proiect_poo/GeneratorSpeaker.java
+++ Proiect_POO/src/proiect_poo/GeneratorSpeaker.java
@@ -17,10 +17,10 @@ public class GeneratorSpeaker {
     static private int[] stoc = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     static private boolean[] onTheEar = { true, false };
+    static private boolean[] hasMicrofon = { true, false };
 
-    static private String[] operationalSystem = { "Windows", "IOS", "Android" };
+    static private boolean[] operationalSystem = { true,false};
 
-    static private boolean[] microfon = { true, false };
 
     static private int[] frequency = { 2600, 2666, 3200, 3666 };
 
@@ -31,20 +31,19 @@ public class GeneratorSpeaker {
         for (int i = 0; i < n; i++) {
             String brandSpeaker = brand[new Random().nextInt(brand.length)];
             String culoareSpeaker = culoare[new Random().nextInt(culoare.length)];
-            String stareSpeaker = stare[new Random().nextInt(stare.length)];
+            String stareSpeaker = stare[new Random().nextInt(stare.length)]; 
             int garantieSpeaker = garantie[new Random().nextInt(garantie.length)];
             float pretSpeaker = pret[new Random().nextInt(pret.length)];
             int stocSpeaker = stoc[new Random().nextInt(stoc.length)];
             boolean onTheEarSpeaker = onTheEar[new Random().nextInt(onTheEar.length)];
-            boolean microfonSpeaker = microfon[new Random().nextInt(microfon.length)];
-            String operationalSystemSpeaker = operationalSystem[new Random().nextInt(operationalSystem.length)];
+            boolean hasMicrofonSpeaker = hasMicrofon[new Random().nextInt(hasMicrofon.length)];
+            boolean operationalSystemSpeaker = operationalSystem[new Random().nextInt(operationalSystem.length)];
             int frequencySpeaker = frequency[new Random().nextInt(frequency.length)];
             int chargingTimeSpeaker = chargingTime[new Random().nextInt(chargingTime.length)];
 
-            Speaker speaker = new Speaker(brandSpeaker, culoareSpeaker, stareSpeaker, garantieSpeaker, pretSpeaker, stocSpeaker, onTheEarSpeaker,
-            microfonSpeaker, operationalSystemSpeaker, chargingTimeSpeaker);
-            listaSpeaker[i] = speaker;
+            Speaker speaker = new Speaker(brandSpeaker,culoareSpeaker,stareSpeaker,garantieSpeaker,pretSpeaker,stocSpeaker,onTheEarSpeaker,hasMicrofonSpeaker,operationalSystemSpeaker,frequencySpeaker,chargingTimeSpeaker);
 
+            listaSpeaker[i]=speaker;
         }
         return listaSpeaker;
     }
